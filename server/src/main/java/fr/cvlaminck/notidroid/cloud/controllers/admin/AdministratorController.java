8b6a639afc6647b7bd8a274fb5a851c651d11976
Cyril Vlaminck <developer@cvlaminck.fr>
2014-04-09 23:25:06 +0200

    Added form to register new Administrator

---
+++ server/src/main/java/fr/cvlaminck/notidroid/cloud/controllers/admin/AdministratorController.java
@@ -0,0 +1,28 @@
+package fr.cvlaminck.notidroid.cloud.controllers.admin;
+
+import fr.cvlaminck.notidroid.cloud.data.entities.Administrator;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin/administrators")
+public class AdministratorController extends MasterController {
+
+    public AdministratorController() {
+
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public ModelAndView formAddNewAdministrator() {
+        return new ModelAndView("administrators.add", "newAdministrator", new Administrator());
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public ModelAndView addNewAdministrator(@ModelAttribute Administrator newAdministrator) {
+        return new ModelAndView("administrators.add", "newAdministrator", newAdministrator);
+    }
+
+}
--- server/src/main/java/fr/cvlaminck/notidroid/cloud/data/entities/Administrator.java
+++ server/src/main/java/fr/cvlaminck/notidroid/cloud/data/entities/Administrator.java
@@ -1,5 +1,7 @@
 package fr.cvlaminck.notidroid.cloud.data.entities;
 
+import org.springframework.data.annotation.Id;
+
 /**
  * This class represents an user that can access the administration.
  * Administrator and User are fully separated because they do not have the
@@ -7,4 +9,54 @@ package fr.cvlaminck.notidroid.cloud.data.entities;
  * user because they are fully separated.
  */
 public class Administrator {
+
+    @Id
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    public Administrator() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
